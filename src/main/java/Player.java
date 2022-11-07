8ea292e045707e9f31a44c689e2ccf5ca7fbaf05
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2022-11-07 16:34:20 +0100

    nappet vores Player klasse og ændret lidt i den

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,32 @@
+package main.java;
+
 public class Player {
+
+    private boolean turnIndicator;
+    private int playerFigure;
+
+    /**
+     * @param playerFigure 1-4
+     */
+    public Player(int playerFigure) {       //constructor
+        this.turnIndicator = false;
+        this.playerFigure = playerFigure;
+    }
+    Account account = new Account();
+
+    public int getBalance() {       //getter
+        return account.getBalance();
+    }
+
+    public void setBalance(int balance) {       //setter
+        //mangler method body
+    }
+
+    public void setTurnIndicator(boolean turnIndicator) {       //sets turnIndicator to either true or false
+        this.turnIndicator = turnIndicator;
+    }
+
+    public boolean isTurnIndicator() {     //gets turnIndicator
+        return turnIndicator;
+    }
 }

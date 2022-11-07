4684a796b63c5645f690c4a197469f31200dd621
s205793 <s205793@dtu.dk>
2022-11-07 22:57:01 +0100

    Wrote code in class Cards. Added classes for different cards.

--- src/main/java/chance/Cards.java
+++ src/main/java/chance/Cards.java
@@ -1,4 +1,14 @@
 package chance;
-
 public class Cards {
+
+    public String desc;
+    public HashMap<String, Integer> actions;
+
+    Cards (String desc) {
+        this.desc = desc;
+        actions = new HashMap<String, Integer>();
+    }
+    public addAction(String action, int value) {
+        actions.put(action, value);
+    }
 }

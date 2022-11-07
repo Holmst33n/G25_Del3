510363f814758a7c03fc4ff84c3acadb9308848f
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2022-11-07 19:13:55 +0100

    begun construction of GameBoard class

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,2 +1,14 @@
+package main.java;
 public class GameBoard {
+
+    private int playerAmount;
+
+    /**
+     * @param players amount of players
+     */
+    public GameBoard(int players){
+        this.playerAmount = players;
+    }
+
+
 }

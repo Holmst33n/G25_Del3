31473d4dd1bb2795787c97b11f8f9f004d77732d
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2022-11-08 19:52:07 +0100

    First iteration of jail is done

--- src/main/java/fields/Jail.java
+++ src/main/java/fields/Jail.java
@@ -1,13 +1,16 @@
 package fields;
 public class Jail {
-
-    int bail = 1;
+    int bail = 0;
     int[] playersInJail = new int[4];
-    public int freePlayer(Boolean isInjail){
-
-        if(isInjail)
-            isInjail = false;
-            return bail;
+    public int freePlayer(Boolean isInJail){
+        if(isInJail == null){
+            System.out.println("Error: Player isInJail status unknown");
+        }
+        if(isInJail){
+            isInJail = false;
+            bail = 1;
+        }
+        return bail;
     }
 
     public void setPlayersInJail(int[] playersInJail) {

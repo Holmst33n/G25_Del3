87fbf97f9125ce1b66402360d9695d94dfdaf727
s205793 <s205793@dtu.dk>
2022-11-22 19:24:14 +0100

    Fixed small error in Cards class

--- src/main/java/chance/Cards.java
+++ src/main/java/chance/Cards.java
@@ -1,8 +1,29 @@
 package chance;
-import gui_main.GUI;
-
+import java.util.HashMap;
 public class Cards {
-    public static void main(String[] args) {
-        GUI gui = new GUI();
+    String description;
+    int value;
+
+    Cards(String desc, int value) {
+        this.description = desc;
+        this.value = value;
     }
+    public String getDescription(){
+    }
+    public int getValue(){
+    }
+    //Idéen med koden her er at:
+    //Overstående skal bruges til basale kort med én handling
+    //Understående skal bruges til kort med flere handlinger f.eks. Ryk x felter tilbage og betal x penge
+    public String text;
+    public HashMap<String, Integer> actions;
+    Cards(String text) {
+        this.text = text;
+        actions = new HashMap<String, Integer>();
+    }
+    addAction(String action, int value) {
+        actions.put(action, value);
+    }
+    //Der skal tilføjes cardDraw metode her
+
 }

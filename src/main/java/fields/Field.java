9172775702b208a0753e3926d7af8cccb4ece459
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2022-11-22 21:11:03 +0100

    added a landOnField method to Field.java finalized Jail.java

--- src/main/java/fields/Field.java
+++ src/main/java/fields/Field.java
@@ -7,24 +7,27 @@ public class Field extends Fields {
     int cost; // Price of the Field, when bought by a player
     int rent; // The fee competing players will have to pay if the land on the field
     boolean isOwned = false;
-    public Field (String fieldName, int fieldID, int owner, int cost, int rent){
+
+    public Field(String fieldName, int fieldID, int owner, int cost, int rent) {
         this.fieldName = fieldName;
         this.fieldID = fieldID;
         this.owner = owner;
         this.cost = cost;
         this.rent = rent;
     }
-    public int buyField(int playerID){
-        owner = playerID;
-        isOwned = true;
-        return cost;
-    }
-    public int landOnOwnedField(){
-        int feeToBePaid = 0;
-        if (isOwned){
-            feeToBePaid = rent;
-        }
-        return feeToBePaid;
-    }
+    public int landOnField(int playerId) {
+        int financialConsequence = 0;
 
+        if (playerId != this.owner) {
+            if (isOwned) {
+                financialConsequence = rent;
+            }
+            if (!isOwned) {
+                owner = playerId;
+                isOwned = true;
+                financialConsequence = cost;
+            }
+        }
+        return financialConsequence;
+    }
 }

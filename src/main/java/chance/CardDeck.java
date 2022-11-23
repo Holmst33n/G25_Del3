ffef40525c050bc326e2f3d44ed2f0b9ae3106d4
s205793 <s205793@dtu.dk>
2022-11-23 11:47:00 +0100

    Added additional chance cards in Class ChanceDeck Removed ACT_GO_TO_JAIL from Class CardDeck

--- src/main/java/chance/CardDeck.java
+++ src/main/java/chance/CardDeck.java
@@ -5,20 +5,24 @@ import java.util.Collections;
 
 public class CardDeck {
 
-    //ACT står for action
+    //ACT is short for action
+    //Action objects
     public static final int ACT_MOVE = 0;
     public static final int ACT_RECEIVE = 1;
     public static final int ACT_PAY = 2;
-    public static final int ACT_GO_TO_JAIL = 3;
-    public static final int ACT_LEAVE_JAIL = 4;
-    public static final int ACT_MOVE_FORWARD = 5;
+    public static final int ACT_LEAVE_JAIL = 3;
+    public static final int ACT_MOVE_FORWARD = 4;
 
+    //Array of cards
     ArrayList<Cards> cards = new ArrayList<Cards>();
 
+    //Method for adding chance cards
     public void add (Cards card) {
         cards.add(card);
         return;
     }
+
+    //Method for shuffling the 'deck' of cards
     public void shuffle () {
         Collections.shuffle(cards);
         return;
--- src/main/java/chance/Cards.java
+++ src/main/java/chance/Cards.java
@@ -1,7 +1,7 @@
 package chance;
-import java.util.HashMap;
 public class Cards {
 
+    //Objects to define chance cards
     private int type;
     private String desc;
     private int action;
@@ -11,12 +11,14 @@ public class Cards {
         desc = "";
         return;
     }
+    //Method for simple chance cards
     Cards (int type, String desc, int action) {
         this.type = type;
         this.desc = desc;
         this.action = action;
         return;
     }
+    //Method for chance cards involving currency
     Cards (int type, String desc, int action, int parameter) {
         this.type = type;
         this.desc = desc;
@@ -24,6 +26,7 @@ public class Cards {
         this.parameter = parameter;
         return;
     }
+    //Getters
     public int getType () {
         return type;
     }
